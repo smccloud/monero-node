1aa7fb39e0edecdbd077c2202e2cacdf5ca9af2a
Shaun McCloud <git@smccloud.com>
2017-10-08 21:53:13 -0500

    Fixed error in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y  nginx
+apt-get install -y  nginx wget
 wget -O /monero-linux-x64-v0.11.0.0.tar.bz2 https://downloads.getmonero.org/cli/monero-linux-x64-v0.11.0.0.tar.bz2
 #tar xvf /monero-linux-x64-v0.11.0.0.tar.bz2
