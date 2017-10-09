c4939ba1911e6ec2bf808ba947aa016ad5f4f61a
Shaun McCloud <git@smccloud.com>
2017-10-08 22:12:40 -0500

    Remove --detach from monerod command

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /monero-v0.11.0.0/blockchain
-/monero-v0.11.0.0/monerod --data-dir=/monero-v0.11.0.0/blockchain --block-sync-size=20 --rpc-bind-ip 0.0.0.0 --restricted-rpc --confirm-external-bind --detach
+/monero-v0.11.0.0/monerod --data-dir=/monero-v0.11.0.0/blockchain --block-sync-size=20 --rpc-bind-ip 0.0.0.0 --restricted-rpc --confirm-external-bind
