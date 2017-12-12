992abac62fe04b5444496565ff17e004c1717899
Shaun McCloud <shaun.thomas.mccloud@gmail.com>
2017-12-12 12:05:01 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /monero/blockchain
-/monero/build/release/bin/monerod --data-dir=/monero/blockchain --block-sync-size=20 --rpc-bind-ip 0.0.0.0 --restricted-rpc --confirm-external-bind
+/monero/monerod --data-dir=/monero/blockchain --block-sync-size=20 --rpc-bind-ip 0.0.0.0 --restricted-rpc --confirm-external-bind
