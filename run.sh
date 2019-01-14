a13502ebb27e3fefcb1ad673dffe5578deaa29a1
Shaun McCloud <shaun.thomas.mccloud@gmail.com>
2019-01-14 10:35:49 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /monero/blockchain
-/monero/monerod --data-dir=/blockchain --block-sync-size=20 --rpc-bind-ip 0.0.0.0 --restricted-rpc --confirm-external-bind --detach
+/monero/monerod --data-dir=/blockchain --block-sync-size=20 --rpc-bind-ip 0.0.0.0 --restricted-rpc --confirm-external-bind --detach --log-file=/blockchain/bitmonero.log
